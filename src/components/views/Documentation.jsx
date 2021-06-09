5be0c534b8a7d376c42c3083a7c53764ff8e74c6
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-06-09 10:30:36 +0200

    correction ortho

--- src/components/views/Documentation.jsx
+++ src/components/views/Documentation.jsx
@@ -4,9 +4,9 @@ const Documentation = () => {
   const docInfo = {
     image:
       'https://cdn.pixabay.com/photo/2017/03/10/17/04/physiotherapy-2133286_960_720.jpg',
-    name: 'Le pieds dans tous ses états',
+    name: 'Le pied dans tous ses états',
     description:
-      'Découvrez le pied à travers les ages; comment le soignaient les kinésythérapeutes du XIXe siècle à nos jours',
+      'Découvrez le pied à travers les âges; comment le soignaient les kinésithérapeutes du XIXe siècle à nos jours.',
     author: 'Emile Latouche',
     price: '10,40',
   };
@@ -17,49 +17,49 @@ const Documentation = () => {
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
       <div className="doc-container">
         <img src={docInfo.image} className="image" alt="doc pieds" />
         <h2 className="titre">{docInfo.name}</h2>
-        <p className="author">{docInfo.author}</p>
+        <p className="author">par {docInfo.author}</p>
         <p className="description">{docInfo.description}</p>
         <p className="price">{docInfo.price} €</p>
       </div>
