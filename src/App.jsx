45f89ae1a742c97be75ee4ebeb99a7322f60a08c
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-07-07 10:38:55 +0200

    début de la fonction rechercher

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GeneralRouter from './components/commons/GeneralRouter';
+import GlobalSearch from './components/views/GlobalSearch';
 
 function App() {
   return (
     <div className="App">
       <GeneralRouter />
+      <GlobalSearch />
     </div>
   );
 }
--- src/components/views/GlobalSearch.jsx
+++ src/components/views/GlobalSearch.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function GlobalSearch() {
+  const [search, setSearch] = useState('');
+  axios
+    .post(`${process.env.REACT_APP_BACKEND_URL}/globalSearch`, { search })
+    .then((results) => {
+      console.log(results);
+    });
+  console.log(search);
   return (
     <div>
-      <h1>Global Search</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
     </div>
   );
 }
